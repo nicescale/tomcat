5dd9dc2456b72e69d8ec670ba99207eee384bcc5
William Hanwoody <hanwoody@gmail.com>
2014-07-18 10:50:37 +0800

    support env for tomcat

--- run.sh
+++ run.sh
@@ -2,5 +2,11 @@
 
 cron
 
+envfile=${CONF_DIR-:/etc/tomcat6}/env
+
+[ -f $envfile ] &&
+. $envfile &&
+eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
+
 source /etc/default/tomcat6 && 
 exec /usr/share/tomcat6/bin/catalina.sh run
