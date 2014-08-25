dfe3d7aaa1621fcd1fe612f71b29a82c2eb89831
William Hanwoody <hanwoody@gmail.com>
2014-08-25 10:31:40 +0800

    support jvm setting

--- run.sh
+++ run.sh
@@ -8,5 +8,11 @@ envfile=${CONF_DIR-:/etc/tomcat6}/env
 . $envfile &&
 eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
 
+setenv_file=${CONF_DIR-:/etc/tomcat6}/setenv.sh
+
+[ -f $setenv_file ] &&
+. $setenv_file &&
+eval `cat $setenv_file|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
+
 source /etc/default/tomcat6 && 
 exec /usr/share/tomcat6/bin/catalina.sh run
