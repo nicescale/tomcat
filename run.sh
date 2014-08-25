ef9b3b24bda4f310f317210d9545312b4abb61ef
William Hanwoody <hanwoody@gmail.com>
2014-08-25 12:06:25 +0800

    fix env override problem

--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ cron
 
 envfile=${CONF_DIR-:/etc/tomcat6}/env
 
+source /etc/default/tomcat6
+
 [ -f $envfile ] &&
 . $envfile &&
 eval `cat $envfile|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
@@ -14,5 +16,4 @@ setenv_file=${CONF_DIR-:/etc/tomcat6}/setenv.sh
 . $setenv_file &&
 eval `cat $setenv_file|grep -v ^#|grep '='|cut -f1 -d'='|xargs echo export`
 
-source /etc/default/tomcat6 && 
 exec /usr/share/tomcat6/bin/catalina.sh run
